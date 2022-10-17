1305c87aff8ea3f6207b492368324e00f62b6a90
Andreas <alo102@uit.no>
2022-10-18 00:20:44 +0200

    Possible draft for task

--- Pwn/Ret2win/buffer_overflow.c
+++ Pwn/Ret2win/buffer_overflow.c
@@ -8,7 +8,28 @@ void ignore_me(){
   setvbuf(stderr, NULL, _IONBF, 0);
 }
 
-void call_me_to_win(){
+void photograph(){
+  puts("So you can keep me");
+  puts("Inside the pocket of your ripped jeans");
+  puts("Holding me closer 'til our eyes meet");
+  puts("You won't ever be alone");
+}
+
+void lose_yourself(){
+  puts("You better lose yourself in the music");
+  puts("The moment, you own it, you better never let it go");
+  puts("You only get one shot, do not miss your chance to blow");
+  puts("This opportunity comes once in a lifetime");
+}
+
+void mamma_mia(){
+  puts("Mamma mia, here I go again");
+  puts("My my, how can I resist you?");
+  puts("Mamma mia, does it show again?");
+  puts("My my, just how much I've missed you");
+}
+
+void call_me_maybe(){
   char chr;
   FILE *f = fopen("flag.txt", "r");
 
@@ -22,8 +43,23 @@ void call_me_to_win(){
 
 int main(){
   ignore_me();
-  char message[60];
-  puts("Enter some nostalgic message here:");
-  gets(message);
+  char song[30];
+  puts("Welcome to UiT's MP3 Player!\n");
+  puts("Please choose the name of the song you want to play:\n");
+  puts("1. Photograph - Nickelback\n2. Lose Yourself - Eminem\n3. Mamma Mia - ABBA");
+  gets(song);
+
+  if(!strcmp(song, "Photograph")){
+    photograph();
+  }
+  else if(!strcmp(song, "Lose Yourself")){
+    lose_yourself();
+  }
+  else if(!strcmp(song, "Mamma Mia")){
+    mamma_mia();
+  }
+  else {
+    puts("Could not play the requested song");
+  }
   return 0;
 }
--- Pwn/Ret2win/notes.md
+++ Pwn/Ret2win/notes.md
@@ -1,11 +1,9 @@
 ## In Progress: Current idea of the task and its solution
 
 ## Running the program over a port
-`socat TCP-LISTEN:9001,reuseaddr,fork EXEC:"./test",stderr`
+`socat TCP-LISTEN:9001,reuseaddr,fork EXEC:"./mp3_player",stderr`
 
 ## Missing
-- Probably some design changes (nostalgic)
-- Create flag
 - Create writeup.md
 - Create README for task
-- Docker-support
+- Dockerfile
