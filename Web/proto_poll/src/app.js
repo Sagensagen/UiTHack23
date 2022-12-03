c94be8104730db17b6d9c2bed888efee29915897
Andreas <alo102@uit.no>
2022-12-03 23:52:57 +0100

    Some cleanup

--- Web/proto_poll/src/app.js
+++ Web/proto_poll/src/app.js
@@ -4,7 +4,6 @@ const utils = require("./utils");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use("/static", express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
