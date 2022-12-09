7304f68d4a9217af13e5f4c2431e05126182bbec
Loevland <34869848+Loevland@users.noreply.github.com>
2022-12-09 12:29:39 +0100

    Forrest (#62)
    * Added easy rev task
    
    * Update readme
    
    Co-authored-by: Finn Olav Sagen <34317459+Sagensagen@users.noreply.github.com>

---
+++ Rev_eng/forrest/README.md
@@ -0,0 +1,11 @@
+# Rev - Forrest
+> Rev- 50pts
+
+My mama always said, "Binaries are not great for hiding things".
+
+
+Hint 1. Try looking for text strings
+
+Files: [Binary](src/forrest)
+
+[Writeup](writeup.md)
---
+++ Rev_eng/forrest/src/forrest.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  char *flag = "UiTHack23{L1f3_w4s_lik3_4_b0x_0f_ch0col47e5}";
+  printf("My mama always said, 'Binaries are not great for hiding things'\n");
+  return 0;
+}
