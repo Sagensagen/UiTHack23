01342fdf3de0808dbc619f7a7c13b2626935e779
alex <alexstim@hotmail.no>
2022-12-12 15:43:46 +0100

    Ancient Pete working :alembic:

---
+++ Web/ancient_machine/ancient_machine.py
@@ -0,0 +1,70 @@
+import json
+from datetime import datetime
+from itertools import groupby
+
+from flask import Flask, request, jsonify
+from flask_apscheduler import APScheduler
+
+START = -1
+END = -1
+
+app = Flask(__name__)
+
+password = []
+password_correct = ["2", "4", "6", "8", "10"]
+
+
+def clear_password():
+    # Clear password every second'
+    global START
+    global END
+    START = -1
+    END = -1
+
+    if len(password) > 0:
+        password.clear()
+
+
+@app.route('/', methods=['GET'])
+def attempt_connection():
+
+    global START
+
+    data = request.args.get('digit')
+
+    # Get end time
+    if START != -1:
+        global END
+        now = datetime.now()
+        END = now.second
+        password.append(data)
+        if (END - START) < 1 and password == password_correct:
+            START = -1
+            END = -1
+            password.clear()
+            return jsonify({'key': 'you-found-the-flag'})
+        elif (END - START) >= 1:
+            START = -1
+            END = -1
+            password.clear()
+            return 'TOO SLOW, RESTARTING\n'
+        else:
+            now = datetime.now()
+            START = now.second
+            return 'HURRY\n'
+
+    # Get start time
+    else:
+        password.clear()
+        now = datetime.now()
+        START = now.second
+        password.append(data)
+        return 'STARTING, HURRY!\n'
+
+
+if (__name__ == "__main__"):
+    scheduler = APScheduler()
+    scheduler.add_job(func=clear_password,
+                      trigger='interval', id='job', seconds=1)
+    scheduler.start()
+    app.run(debug=True)
