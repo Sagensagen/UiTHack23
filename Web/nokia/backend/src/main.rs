e2dd5df5b2abb19098412882a28ae4f7ef49d747
Jørgen Kristensen <jk@bitnor.org>
2023-01-06 14:21:38 +0100

    add cors to respones to allow any origin

--- Web/nokia/backend/src/main.rs
+++ Web/nokia/backend/src/main.rs
@@ -46,8 +46,11 @@ use serde::Deserialize;
 use serde_json::json;
 use std::{collections::HashMap, sync::Arc, time::Duration, time::Instant};
 use tokio::sync::RwLock;
-use tower_http::trace::{DefaultOnRequest, DefaultOnResponse, TraceLayer};
 use tower_http::LatencyUnit;
+use tower_http::{
+    cors::{self, CorsLayer},
+    trace::{DefaultOnRequest, DefaultOnResponse, TraceLayer},
+};
 use tracing::Level;
 use tracing_subscriber::{prelude::*, EnvFilter};
 use uuid::Uuid;
@@ -71,6 +74,8 @@ async fn main() {
         .route("/message/:id/send", post(message_send))
         .route("/button/:btn", post(button_press))
         .with_state(db)
+        .layer(DefaultBodyLimit::max(1024))
+        .layer(CorsLayer::new().allow_origin(cors::Any))
         .layer(
             TraceLayer::new_for_http()
                 .on_request(DefaultOnRequest::new().level(Level::INFO))
@@ -79,8 +84,7 @@ async fn main() {
                         .level(Level::INFO)
                         .latency_unit(LatencyUnit::Micros),
                 ),
-        )
-        .layer(DefaultBodyLimit::max(1024));
+        );
 
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
