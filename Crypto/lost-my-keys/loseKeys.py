8305d8b846a94fc6e88e18fb1a5773085286e3e4
marius <min016@uit.no>
2023-02-11 17:16:32 +0100

    lose file to make public

--- Crypto/lost-my-keys/loseKeys.py
+++ Crypto/lost-my-keys/loseKeys.py
@@ -1,41 +1,31 @@
+import os
 from   pickle import dumps
 from   sys import argv
 
-def mkjungle(J:"list[str]", K:"list[str]", start:str) -> "dict[str:dict]":
-	""" 
-	Create directories from strings in J.
-	Directory names are XOR'ed with rotating strings in K. 
-	Number of directories created will be len(J)**2.
-	"""
+def mkjungle(J:"list[str]", K:"list[str]", start:str) -> "list[str]":
 	for i in range(len(J)):
 		J[i] = E(J[i],K[i%len(K)])
-	return _mkjungle(J,{},start)
+	return _mkjungle(J,[ start ],start)
 
-def _mkjungle(J:"list[str]", jungle:"dict[str:dict]", here:str, n:int=0) -> "dict[str:dict]":
-	jungle[here] = { J[i] : {} for i in range(n,len(J)) }
+def _mkjungle(J:"list[str]", jungle:"list[str]", here:str, n:int=0) -> "list[str]":
+	jungle += [ os.path.join(here,J[i]) for i in range(len(J)) ]
 	for i in range(n,len(J)):
-		_mkjungle(J,jungle[here],J[i],n+1)
+		_mkjungle(J,jungle,os.path.join(here,J[i]),n+1)
 	return jungle
 
 def E(p:str, q:str) -> str:
-	""" Return XOR with p and q as string. """
 	return str(B(p)^B(q))
 
 def B(s:str) -> int:
-	""" Return string as byte-value. """
 	return int.from_bytes(s.encode(),byteorder="big")
 
 if __name__ == "__main__":
 	if len(argv) != 3:
 		exit(f"usage: python {__file__} <key-file> <jungle-file> ")
 
-	jungle, parent, (KFile,JFile) = "jungle-of-keys", "..", argv[1:]
+	jungle, (KFile,JFile) = "jungle-of-keys.bin", argv[1:]
 	
 	J = open(JFile).readlines()
 	K = open(KFile).read().split('-')
-	with open(jungle+".bin","wb") as j:
+	with open(jungle,"wb") as j:
 		j.write( dumps( mkjungle(J,K,jungle) ) )
-
-	# TODO: 
-	# make less readable version
-	# make solution script
